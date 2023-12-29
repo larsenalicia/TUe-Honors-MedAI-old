95398588428b8f06289589953a4dffcb2c54bfa1
PhatLeBao-Phat <104715759+PhatLeBao-Phat@users.noreply.github.com>
2023-12-29 21:16:43 +0100

    Multitaper spectral analysis

--- signal_analysis/feature_extraction.py
+++ signal_analysis/feature_extraction.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from abc import ABC, abstractmethod 
 from scipy.fft import rfft, rfftfreq
+from mne.time_frequency import psd_array_multitaper
+from typing import Optional
 
 class FeatureExtraction(ABC):
     """Store Feature Extraction methods"""
@@ -27,7 +29,7 @@ class FourierTransform(FeatureExtraction):
         self.fs = fs
         self.f_res = f_res
 
-    def transform(self, sample, epoch_size=None):
+    def transform(self, sample: np.ndarray, epoch_size=None):
         """Transform from time domain to frequency domain
         
         Parameters
@@ -56,8 +58,66 @@ class FourierTransform(FeatureExtraction):
 
         return abs(fft_output), freq 
 
-    
-    
+
+class MultitaperSpectralTransform(FeatureExtraction):
+    """ Multitaper Spectral Analysis"""
+
+    def __init__(self, fs=400, fmin=0, fmax=50, epoch_size=2, f_res=0.5):
+        self.fs = fs 
+        self.fmin = fmin 
+        self.fmax = fmax 
+        self.epoch_size = epoch_size 
+        self.f_res = f_res
+
+    def transform(self, sample: np.ndarray, epoch_size=None):
+        """Transofrm from time domain to frequency domain 
+        using Multitaper spectral method 
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        psds: raw PSD estimate for each epoch. For example, if we process with 36 epochs 
+            then `psds.shape = (36, 101)`. 101 are real values for each freqs bin
+        freqs: frequency bins
+
+        Notes
+        -------
+        The input is the whole signal not just 1 epoch. The multitaper method assigned
+        a different taper to different epochs similar to window function.
+        """
+        if epoch_size:
+            self.epoch_size = epoch_size
+        n = len(sample)
+        n_per_epoch = self.epoch_size * self.fs
+        n_epoch = (n // n_per_epoch)
+        X = sample[:n_epoch * n_per_epoch].reshape(n // n_per_epoch, n_per_epoch)
+
+        psds, freqs = psd_array_multitaper(X, self.fs, fmin=self.fmin, 
+                                           fmax=self.fmax, n_jobs=1, output="power")
+
+        self.psds = psds 
+        self.freqs = freqs 
+
+        return psds, freqs
 
 
+class BandwisePowerTransform(FeatureExtraction):
+    """Extract bandwise power in canonical spectral ranges"""
+    
+    def transform(self, psds: np.ndarray | MultitaperSpectralTransform | FourierTransform, 
+                freqs: Optional[np.ndarray]):
+        if isinstance(psds, np.ndarray):
+            NotImplemented
         
+        if isinstance(psds, MultitaperSpectralTransform):
+            NotImplemented
+        
+        if isinstance(psds, FourierTransform):
+            NotImplemented
+    
+
+    def raw_transform():
+        NotImplemented
+    
