f4f97c3f619fe7d5d4ddec286bce3797173b3f98
PhatLeBao-Phat <104715759+PhatLeBao-Phat@users.noreply.github.com>
2024-02-03 12:28:37 +0100

    final

---
+++ signal_analysis/pipeline.py
@@ -0,0 +1,11 @@
+# Internal import 
+from .feature_extraction import *
+
+class Pipeline(object):
+    """Define whole pipeline"""
+
+    def __init__(self) -> None:
+        NotImplemented
+    
+    def add_stage(self) -> None:
+        NotImplemented
